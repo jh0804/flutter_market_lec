a8696558b0e3d16c7f1cc4dfd273d29197b6aa19
JH <wngml0708@gmail.com>
2025-06-09 03:36:56 +0900

    채팅 화면 만들기

--- lib/screens/chatting/chatting_screen.dart
+++ lib/screens/chatting/chatting_screen.dart
@@ -1,13 +1,26 @@
+import 'package:carrot_app/models/chat_message.dart';
+import 'package:carrot_app/screens/chatting/components/appbar_preferred_size.dart';
+import 'package:carrot_app/screens/chatting/components/chat_container.dart';
 import 'package:flutter/material.dart';
 
 class ChattingScreen extends StatelessWidget {
+
+  const ChattingScreen();
+
   @override
   Widget build(BuildContext context) {
     print("ChattingScreen build");
     return Scaffold(
-      body: Center(child: Text("ChattingScreen")),
+      appBar: AppBar(
+        title: Text("채팅"),
+        bottom: appBarBottomLine(),
+      ),
+      body: ListView(
+        children: List.generate(
+          chatMessageList.length,
+          (index) => ChatContainer(chatMessage : chatMessageList[index]),
+        ),
+      ),
     );
   }
-
-  const ChattingScreen();
 }
