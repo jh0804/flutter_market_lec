43c14681df79cba568b180256c2d7cb9a011da7d
JH <wngml0708@gmail.com>
2025-06-09 04:04:04 +0900

    동네생활 화면 만들기 - LifeBody 위젯 영역 잡기

---
+++ lib/screens/neighborhood_life/components/life_body.dart
@@ -0,0 +1,93 @@
+import 'package:carrot_app/models/neighborhood_life.dart';
+import 'package:flutter/material.dart';
+
+class LifeBody extends StatelessWidget{
+
+  final NeighborhoodLife neighborhoodLife;
+
+  const LifeBody({Key? key, required this.neighborhoodLife}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          bottom: BorderSide(width: 0.5, color: Color(0xFFD4D5DD)),
+        ),
+      ),
+      child: Column(
+        children: [
+          _buildTop(),
+          _buildWriter(),
+          _buildWriting(),
+          _buildImage(),
+          Divider(
+            height: 1,
+            thickness: 1,
+            color: Colors.grey,
+          ),
+          _buildTail(neighborhoodLife.commentCount),
+        ],
+      ),
+    );
+  }
+
+  Padding _buildTop(){
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        vertical: 16,
+        horizontal: 16,
+      ),
+      child: Container(
+        color: Colors.orange[100], height: 30,
+      ),
+    );
+  }
+
+  Padding _buildWriter(){
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: 16,
+      ),
+      child: Container(
+        color: Colors.red[100], height: 30,
+      ),
+    );
+  }
+
+  Padding _buildWriting(){
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        vertical: 16,
+        horizontal: 16,
+      ),
+      child: Container(
+        color: Colors.blue[100], height: 50,
+      ),
+    );
+  }
+
+  Padding _buildImage(){
+    return Padding(
+      padding: EdgeInsets.only(
+        left: 16,
+        right: 16,
+        bottom: 16,
+      ),
+      child: Container(
+        color: Colors.black, height: 150,
+      ),
+    );
+  }
+
+  Padding _buildTail(int commentCount){
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Container(
+        color: Colors.lime[100], height: 50,
+      ),
+    );
+  }
+
+}
--- lib/screens/neighborhood_life/neighborhood_life_screen.dart
+++ lib/screens/neighborhood_life/neighborhood_life_screen.dart
@@ -1,13 +1,26 @@
+import 'package:carrot_app/screens/chatting/components/appbar_preferred_size.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class NeighborhoodLifeScreen extends StatelessWidget {
+  const NeighborhoodLifeScreen();
+
   @override
   Widget build(BuildContext context) {
     print("NeighborhoodLifeScreen build");
     return Scaffold(
-      body: Center(child: Text("NeighborhoodLifeScreen")),
+      appBar: AppBar(
+        title: Text("동네생활"),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.search)),
+          IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.plus_rectangle_on_rectangle)),
+          IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.bell)),
+        ],
+        bottom: appBarBottomLine(),
+      ),
+      body: ListView(
+        children: [],
+      ),
     );
   }
-
-  const NeighborhoodLifeScreen();
 }
